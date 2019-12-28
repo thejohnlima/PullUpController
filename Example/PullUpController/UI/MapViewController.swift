cd91996c4d3e2656b326f2a4e32e6023ce680bd5
John Lima <thejohnlima@icloud.com>
2019-12-28 20:19:17 -0300

    Demo Project
    Remove dead code;
    Improve code style;
    General improvements;

--- Example/PullUpController/UI/MapViewController.swift
+++ Example/PullUpController/UI/MapViewController.swift
@@ -11,60 +11,30 @@ import MapKit
 
 class MapViewController: UIViewController {
 
+    // MARK: - Properties
     @IBOutlet private weak var mapView: MKMapView!
+    @IBOutlet private weak var widthSlider: UISlider!
+    @IBOutlet private weak var initialStateSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var animateShowSwitch: UISwitch!
+    @IBOutlet weak var animateHideSwitch: UISwitch!
+
     @IBOutlet private weak var sizeSliderView: UIView! {
         didSet {
             sizeSliderView.layer.cornerRadius = 10
         }
     }
-    @IBOutlet private weak var widthSlider: UISlider!
-    @IBOutlet private weak var initialStateSegmentedControl: UISegmentedControl!
 
     private var originalPullUpControllerViewSize: CGSize = .zero
-    @IBOutlet weak var animateShowSwitch: UISwitch!
-    @IBOutlet weak var animateHideSwitch: UISwitch!
-    
-    private func makeSearchViewControllerIfNeeded() -> SearchViewController {
-        let currentPullUpController = children
-            .filter({ $0 is SearchViewController })
-            .first as? SearchViewController
-        let pullUpController: SearchViewController = currentPullUpController ?? UIStoryboard(name: "Main",bundle: nil).instantiateViewController(withIdentifier: "SearchViewController") as! SearchViewController
-        if initialStateSegmentedControl.selectedSegmentIndex == 0 {
-            pullUpController.initialState = .contracted
-        } else {
-            pullUpController.initialState = .expanded
-        }
-        if originalPullUpControllerViewSize == .zero {
-            originalPullUpControllerViewSize = pullUpController.view.bounds.size
-        }
 
-        return pullUpController
-    }
-
+    // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         addPullUpController(animated: animateShowSwitch.isOn)
     }
 
-    private func addPullUpController(animated: Bool) {
-        let pullUpController = makeSearchViewControllerIfNeeded()
-        _ = pullUpController.view // call pullUpController.viewDidLoad()
-        addPullUpController(pullUpController,
-                            initialStickyPointOffset: pullUpController.initialPointOffset,
-                            animated: animated)
-    }
-
-    func zoom(to location: CLLocationCoordinate2D) {
-        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        let region = MKCoordinateRegion(center: location, span: span)
-
-        mapView.setRegion(region, animated: true)
-    }
-
+    // MARK: - Actions
     @IBAction private func addButtonTapped() {
-        guard
-            children.filter({ $0 is SearchViewController }).count == 0
-            else { return }
+        guard children.first(where: { $0 is SearchViewController }) == nil else { return }
         addPullUpController(animated: animateShowSwitch.isOn)
     }
 
@@ -74,15 +44,57 @@ class MapViewController: UIViewController {
     }
 
     @IBAction private func widthSliderValueChanged(_ sender: UISlider) {
-        let pullUpController = makeSearchViewControllerIfNeeded()
         let width = originalPullUpControllerViewSize.width * CGFloat(sender.value)
-        pullUpController.portraitSize = CGSize(width: width,
-                                               height: pullUpController.portraitSize.height)
-        pullUpController.landscapeFrame = CGRect(origin: pullUpController.landscapeFrame.origin,
-                                                 size: CGSize(width: width,
-                                                              height: pullUpController.landscapeFrame.height))
+
+        let pullUpController = makeSearchViewControllerIfNeeded()
+        pullUpController.portraitSize = CGSize(width: width, height: pullUpController.portraitSize.height)
+
+        pullUpController.landscapeFrame = CGRect(
+            origin: pullUpController.landscapeFrame.origin,
+            size: CGSize(width: width, height: pullUpController.landscapeFrame.height)
+        )
+
         pullUpController.updatePreferredFrameIfNeeded(animated: true)
     }
 
-}
+    // MARK: - Private Methods
+    private func makeSearchViewControllerIfNeeded() -> SearchViewController {
+        let currentPullUpController = children
+            .filter({ $0 is SearchViewController })
+            .first as? SearchViewController
 
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let newController = storyboard
+            .instantiateViewController(withIdentifier: "SearchViewController") as! SearchViewController
+
+        let pullUpController = currentPullUpController ?? newController
+
+        if initialStateSegmentedControl.selectedSegmentIndex == 0 {
+            pullUpController.initialState = .contracted
+        } else {
+            pullUpController.initialState = .expanded
+        }
+
+        if originalPullUpControllerViewSize == .zero {
+            originalPullUpControllerViewSize = pullUpController.view.bounds.size
+        }
+
+        return pullUpController
+    }
+
+    private func addPullUpController(animated: Bool) {
+        let pullUpController = makeSearchViewControllerIfNeeded()
+        _ = pullUpController.view
+
+        addPullUpController(pullUpController,
+                            initialStickyPointOffset: pullUpController.initialPointOffset,
+                            animated: animated)
+    }
+
+    // MARK: - Public Methods
+    func zoom(to location: CLLocationCoordinate2D) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+}
--- Example/PullUpController/UI/SearchViewController.swift
+++ Example/PullUpController/UI/SearchViewController.swift
@@ -12,71 +12,121 @@ import PullUpController
 
 class SearchViewController: PullUpController {
     
-    enum InitialState {
-        case contracted
-        case expanded
-    }
-    
-    var initialState: InitialState = .contracted
-    
-    // MARK: - IBOutlets
-    
+    // MARK: - Properties
     @IBOutlet private weak var visualEffectView: UIVisualEffectView!
     @IBOutlet private weak var searchBoxContainerView: UIView!
-    @IBOutlet private weak var searchSeparatorView: UIView! {
-        didSet {
-            searchSeparatorView.layer.cornerRadius = searchSeparatorView.frame.height/2
-        }
-    }
     @IBOutlet private weak var firstPreviewView: UIView!
     @IBOutlet private weak var secondPreviewView: UIView!
     @IBOutlet private weak var tableView: UITableView!
-    
-    var initialPointOffset: CGFloat {
-        switch initialState {
-        case .contracted:
-            return searchBoxContainerView?.frame.height ?? 0
-        case .expanded:
-            return pullUpControllerPreferredSize.height
+
+    @IBOutlet private weak var searchSeparatorView: UIView! {
+        didSet {
+            searchSeparatorView.layer.cornerRadius = searchSeparatorView.frame.height / 2
         }
     }
 
     private var locations = [(title: String, location: CLLocationCoordinate2D)]()
-    
-    public var portraitSize: CGSize = .zero
-    public var landscapeFrame: CGRect = .zero
-    
-    // MARK: - Lifecycle
-    
+    var portraitSize: CGSize = .zero
+    var landscapeFrame: CGRect = .zero
+    var initialState: InitialState = .contracted
+
+    var initialPointOffset: CGFloat {
+        let margin: CGFloat = 40
+        var result: CGFloat
+
+        switch initialState {
+        case .contracted:
+            result = searchBoxContainerView?.frame.height ?? 0
+            result += margin
+        case .expanded:
+            result = pullUpControllerPreferredSize.height
+        }
+
+        return result
+    }
+
+    // MARK: - Enums
+    enum InitialState {
+        case contracted
+        case expanded
+    }
+
+    // MARK: - View LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        portraitSize = CGSize(width: min(UIScreen.main.bounds.width, UIScreen.main.bounds.height),
-                              height: secondPreviewView.frame.maxY)
+
+        let width = min(UIScreen.main.bounds.width, UIScreen.main.bounds.height)
+
+        portraitSize = CGSize(width: width, height: secondPreviewView.frame.maxY)
         landscapeFrame = CGRect(x: 5, y: 50, width: 280, height: 300)
-        
         tableView.attach(to: self)
         setupDataSource()
     }
 
+    // MARK: - Overrides
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
         view.layer.cornerRadius = 12
     }
     
     override func pullUpControllerWillMove(to stickyPoint: CGFloat) {
-//        print("will move to \(stickyPoint)")
+        // print("will move to \(stickyPoint)")
     }
     
     override func pullUpControllerDidMove(to stickyPoint: CGFloat) {
-//        print("did move to \(stickyPoint)")
+        // print("did move to \(stickyPoint)")
     }
     
     override func pullUpControllerDidDrag(to point: CGFloat) {
-//        print("did drag to \(point)")
+        // print("did drag to \(point)")
     }
-    
+
+    override var pullUpControllerPreferredSize: CGSize {
+        return portraitSize
+    }
+
+    override var pullUpControllerPreferredLandscapeFrame: CGRect {
+        return landscapeFrame
+    }
+
+    override var pullUpControllerMiddleStickyPoints: [CGFloat] {
+        switch initialState {
+        case .contracted:
+            return [firstPreviewView.frame.maxY]
+        case .expanded:
+            return [searchBoxContainerView.frame.maxY, firstPreviewView.frame.maxY]
+        }
+    }
+
+    override var pullUpControllerBounceOffset: CGFloat {
+        return 20
+    }
+
+    override func pullUpControllerAnimate(action: PullUpController.Action,
+                                          withDuration duration: TimeInterval,
+                                          animations: @escaping () -> Void,
+                                          completion: ((Bool) -> Void)?) {
+        switch action {
+        case .move:
+            UIView.animate(
+                withDuration: 0.3,
+                delay: 0,
+                usingSpringWithDamping: 0.7,
+                initialSpringVelocity: 0,
+                options: .curveEaseInOut,
+                animations: animations,
+                completion: completion
+            )
+        default:
+            UIView.animate(
+                withDuration: 0.3,
+                animations: animations,
+                completion: completion
+            )
+        }
+    }
+
+    // MARK: - Private Methods
     private func setupDataSource() {
         locations.append(("Rome", CLLocationCoordinate2D(latitude: 41.9004041, longitude: 12.4432921)))
         locations.append(("Milan", CLLocationCoordinate2D(latitude: 45.4625319, longitude: 9.1574741)))
@@ -97,56 +147,10 @@ class SearchViewController: PullUpController {
         locations.append(("Dublin", CLLocationCoordinate2D(latitude: 53.3244431, longitude: -6.3857869)))
         locations.append(("Reykjavik", CLLocationCoordinate2D(latitude: 64.1335484, longitude: -21.9224815)))
     }
-    
-    // MARK: - PullUpController
-    
-    override var pullUpControllerPreferredSize: CGSize {
-        return portraitSize
-    }
-    
-    override var pullUpControllerPreferredLandscapeFrame: CGRect {
-        return landscapeFrame
-    }
-    
-    override var pullUpControllerMiddleStickyPoints: [CGFloat] {
-        switch initialState {
-        case .contracted:
-            return [firstPreviewView.frame.maxY]
-        case .expanded:
-            return [searchBoxContainerView.frame.maxY, firstPreviewView.frame.maxY]
-        }
-    }
-    
-    override var pullUpControllerBounceOffset: CGFloat {
-        return 20
-    }
-    
-    override func pullUpControllerAnimate(action: PullUpController.Action,
-                                          withDuration duration: TimeInterval,
-                                          animations: @escaping () -> Void,
-                                          completion: ((Bool) -> Void)?) {
-        switch action {
-        case .move:
-            UIView.animate(withDuration: 0.3,
-                           delay: 0,
-                           usingSpringWithDamping: 0.7,
-                           initialSpringVelocity: 0,
-                           options: .curveEaseInOut,
-                           animations: animations,
-                           completion: completion)
-        default:
-            UIView.animate(withDuration: 0.3,
-                           animations: animations,
-                           completion: completion)
-        }
-    }
-    
 }
 
 // MARK: - UISearchBarDelegate
-
 extension SearchViewController: UISearchBarDelegate {
-    
     func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
         if let lastStickyPoint = pullUpControllerAllStickyPoints.last {
             pullUpControllerMoveToVisiblePoint(lastStickyPoint, animated: true, completion: nil)
@@ -158,31 +162,22 @@ extension SearchViewController: UISearchBarDelegate {
     }
 }
 
+// MARK: - UITableViewDataSource and UITableViewDelegate
 extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
-    
-    // MARK: - UITableViewDataSource
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return locations.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard
-            let cell = tableView.dequeueReusableCell(withIdentifier: "SearchResultCell",
-                                                     for: indexPath) as? SearchResultCell
-            else { return UITableViewCell() }
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchResultCell", for: indexPath) as! SearchResultCell
         cell.configure(title: locations[indexPath.row].title)
         return cell
     }
-    
-    // MARK: - UITableViewDelegate
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         view.endEditing(true)
         pullUpControllerMoveToVisiblePoint(pullUpControllerMiddleStickyPoints[0], animated: true, completion: nil)
-        
         (parent as? MapViewController)?.zoom(to: locations[indexPath.row].location)
     }
 }
